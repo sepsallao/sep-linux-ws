db970d8edfa2e8b026d14aad6863b2468ce02649
sepsallao <dev@sepsallao.com>
2024-03-30 00:19:45 +0800

    added floorp installation

--- sh/sw_browser.sh
+++ sh/sw_browser.sh
@@ -4,6 +4,11 @@ printf "\e[1;32m ---------------------------------------------------------------
 printf "\e[1;32mBrowser\e[0m\n\n"
 
 # Mozilla Firefox
-sudo apt install -y firefox-esr 
+# sudo apt install -y firefox-esr 
+
+curl -fsSL https://ppa.ablaze.one/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
+sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
+sudo apt update
+sudo apt install floorp
 
 exit
